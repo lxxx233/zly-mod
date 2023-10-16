479d9e74714127b6a97b0a987de3e08cd107d155
lxxx233 <lxicloud233@icloud.com>
2023-10-16 18:14:51 +0800

    Initial commit

---
+++ src/client/java/com/zly/ZlyModClient.java
@@ -0,0 +1,10 @@
+package com.zly;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZlyModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
